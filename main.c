fb0e6a03c870d76d746374e918ed6d1206bcff5a
Danny Ho <113899395+DannyHo1209@users.noreply.github.com>
2023-06-04 01:43:24 +0800

    input function added

--- main.c
+++ main.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
+#include <ctype.h>
+
+
+int toNum(char const* arr, int len) {
+    int num = 0;
+    for(int i = 0;i < len;i++) num = num * 10 + ((int)arr[i] - 48);
+    return num;
+}
+
 
 int main() {
-    printf("Hello, World!\n");
+    // Input variables
+    char c;
+    char arr[10000] = {};
+    int length = 0;
+    // Data variables
+    int nodes[10000] = {};
+    int index = 0;
+    // While input not end
+    while(1) {
+        scanf("%c", &c);
+        if (c == ' ' && length != 0) {
+            nodes[index] = toNum(arr, length);
+            index++;
+            length = 0;
+        } else if (isdigit(c)) {
+            arr[length] = c;
+            length++;
+        }
+        if (c == '\n') {
+            if (length != 0) {
+                nodes[index] = toNum(arr, length);
+                index++;
+            }
+            break;
+        }
+    }
+
+
     return 0;
 }
